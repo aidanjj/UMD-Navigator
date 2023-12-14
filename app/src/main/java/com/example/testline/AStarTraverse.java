0e2bc52531dfc46035f64b5869d633a3af20193c
Aidan Jerdee <jerde049@umn.edu>
2023-12-14 10:15:02 -0600

    Added JavaDoc comments to AStarTraverse.

--- app/src/main/java/com/example/testline/AStarTraverse.java
+++ app/src/main/java/com/example/testline/AStarTraverse.java
@@ -7,17 +7,31 @@ import java.util.Stack;
 public class AStarTraverse {
     private Graph g = new Graph();
     private HashMap<Vertex,PointDP> locations = new HashMap<>();
+
+    /**
+     * Returns the distance between two vertices in meters
+     * @param v1 The first vertex
+     * @param v2 The second vertex
+     * @return The distance between the two vertices in meters
+     */
     private float getDistance(Vertex v1, Vertex v2){
         PointDP p1 = locations.get(v1);
         PointDP p2 = locations.get(v2);
         float distance = (float)(1.1866*Math.sqrt((p1.getX() - p2.getX())*(p1.getX() - p2.getX())+(p1.getY() - p2.getY())*(p1.getY() - p2.getY())));
         return distance;
     }
-
     public AStarTraverse(Graph g, HashMap<Vertex,PointDP> locations){
         this.g = g;
         this.locations = locations;
     }
+
+    /**
+     * Finds the path between a source and a destination
+     * @param g The graph to be traversed
+     * @param src The starting vertex
+     * @param dst The ending vertex
+     * @return A HashMap containing the traversed vertices paired with their parent vertices.
+     */
     public HashMap<Vertex,Vertex> findPath(Graph g, Vertex src, Vertex dst){
         VertexPriorityQueue openSet = new VertexPriorityQueue();
         VertexPriorityQueue closedSet = new VertexPriorityQueue();
